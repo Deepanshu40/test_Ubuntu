e52009ea8b1d44fe39d56971ae2cd7abc8505a3f
Deepanshu40 <144712703+Deepanshu40@users.noreply.github.com>
2024-05-10 11:05:02 +0530

    Update puppeteer.js

--- utilis/puppeteer.js
+++ utilis/puppeteer.js
@@ -18,7 +18,7 @@ module.exports.puppeteerSession = async () => {
             console.log("HTML content:", htmlContent);
 
 
-            await page.locator('a[href="https://www.tdscpc.gov.in/">https://www.tdscpc.gov.in]').click();
+            await page.locator('a[href="https://www.tdscpc.gov.in"]').click();
             await page.locator('#modalPage').filter(el => el.display==='block').wait();
             // await page.goto('https://contents.tdscpc.gov.in/', { waitUntil: 'networkidle0' });
             htmlContent = await page.content();
