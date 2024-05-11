7f3550d1786fd8cf37517ac20b93b54a2b65439d
Deepanshu40 <144712703+Deepanshu40@users.noreply.github.com>
2024-05-11 07:14:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express  = require('express');
 const app = express();
-const port = 8080;
+const port = 80;
 const {puppeteerSession} = require('./utilis/puppeteer.js');
 const cors = require('cors');
 
